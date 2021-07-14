150992e344edfc729acba46559206a1127f23640
mgbarbieri <mgbarbieri20@gmail.com>
2021-07-14 18:08:48 -0300

    locale pt

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,28 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'views/listings_screen.dart';
 import 'views/auth_screen.dart';
-import 'package:intl/date_symbol_data_local.dart';
+import 'package:syncfusion_localizations/syncfusion_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  initializeDateFormatting().then((_) => runApp(App()));
+  runApp(App());
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        SfGlobalLocalizations.delegate
+      ],
+      supportedLocales: [
+        const Locale('pt'),
+      ],
+      locale: const Locale('pt'),
       title: 'Agenda',
       theme: ThemeData(
         primarySwatch: Colors.blue,
